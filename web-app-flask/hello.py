ee652274caad1a29b45ab0e6c8a5ec5852d6d953
atishayshukla <atishayshukla@gmail.com>
2020-10-17 21:20:16 +0200

    added conditions in template

--- web-app-flask/hello.py
+++ web-app-flask/hello.py
@@ -2,16 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+actions = [
+    {
+        'Tribe': 'OCF',
+        'Squad': 'CIT',
+        'Application': 'CASV',
+        'OS': 'Linux',
+    },
+    {
+        'Tribe': 'OCF',
+        'Squad': 'CIT',
+        'Application': 'CASV',
+        'OS': 'Linux',
+    },
+
+]
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', actions=actions)
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
