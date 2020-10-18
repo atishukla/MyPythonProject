179e7aeb99416b02b1b072f828b8fa211f059e89
atishayshukla <atishayshukla@gmail.com>
2020-10-18 12:18:56 +0200

    added custom htmls with css for nice looking page

--- web-app-flask/hello.py
+++ web-app-flask/hello.py
@@ -2,27 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-actions = [
+posts = [
     {
-        'Tribe': 'OCF',
-        'Squad': 'CIT',
-        'Application': 'CASV',
-        'OS': 'Linux',
+        'author': 'Atishay Shukla',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
     },
     {
-        'Tribe': 'OCF',
-        'Squad': 'CIT',
-        'Application': 'CASV',
-        'OS': 'Linux',
-    },
-
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
 ]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', actions=actions)
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
