a1343802e80db1a607c13e7c5b37fff762efadb4
atishayshukla <atishayshukla@gmail.com>
2020-10-18 22:05:53 +0200

    adding user credentials in route

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '965e535c3693a8b25d870304ab1f1aa9'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskapp import routes
