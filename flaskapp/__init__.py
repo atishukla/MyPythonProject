08f3ebe14734fa4a7bb93001b08edc49cce4ef49
atishayshukla <atishayshukla@gmail.com>
2020-10-24 21:52:54 +0200

    refactoring-config and app instance

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,31 +1,33 @@
-import os
-
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_sqlalchemy import SQLAlchemy
+from flaskapp.config import Config
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '965e535c3693a8b25d870304ab1f1aa9'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'  # login route for login required decorator
 login_manager.login_message_category = 'info'  # login message in flash format
 
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
-mail = Mail(app)
+mail = Mail()
 
-from flaskapp.users.routes import users
-from flaskapp.posts.routes import posts
-from flaskapp.main.routes import main
 
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    from flaskapp.users.routes import users
+    from flaskapp.posts.routes import posts
+    from flaskapp.main.routes import main
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
