4622982cbde2b61ab93986bb2fa28a724feff0ec
atishayshukla <atishayshukla@gmail.com>
2020-10-24 14:04:47 +0200

    adding password reset functionality

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,7 +1,10 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '965e535c3693a8b25d870304ab1f1aa9'
@@ -11,5 +14,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'  # login route for login required decorator
 login_manager.login_message_category = 'info'  # login message in flash format
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 from flaskapp import routes
